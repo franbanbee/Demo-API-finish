eed7fe7762e5366a1105f533b48db666d8ed2c35
franbanbee <129038669+franbanbee@users.noreply.github.com>
2023-08-29 11:10:14 +0800

    0829-1109 API demo finish thunder client ok

---
+++ src/app/posts/page.js
@@ -0,0 +1,15 @@
+//https://localhost:3001/posts
+
+// import React from 'react';
+
+export const metadata = {
+    title: 'API Demo',
+    description: 'Hi API',
+  }
+  
+const Posts = () =>{
+    return <div>Posts</div>;
+    
+};
+
+export default Posts;
